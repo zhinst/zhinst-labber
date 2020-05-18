80f8772ceefffb89240e7da781be41e481c996a2
Max Ruckriegel <max.ruckriegel@zhinst.com>
2020-05-18 13:02:40 +0200

    Remove some comments and log statements

--- Zurich_Instruments_MFLI/Zurich_Instruments_MFLI.py
+++ Zurich_Instruments_MFLI/Zurich_Instruments_MFLI.py
@@ -14,8 +14,6 @@ class Driver(LabberDriver):
 
     def performOpen(self, options={}):
         """Perform the operation of opening the instrument connection"""
-        # get the interface selected in UI, restrict to either 'USB' or 'PCIE'
-        # TODO: is this valid, is there '1GbE'?
         interface = self.comCfg.interface
         if not interface == "USB":
             interface = "PCIE"
@@ -25,7 +23,6 @@ class Driver(LabberDriver):
         )
         self.controller.setup()
         self.controller.connect_device()
-        # initialize 'Status' quantities
         self.setValue("DAQ - Status", f"Ready for Measurement")
         self.setValue("Sweeper Control - Status", f"Ready for Measurement")
 
@@ -41,92 +38,66 @@ class Driver(LabberDriver):
         """Perform the Set Value instrument operation. This function should
         return the actual value set by the instrument"""
 
-        # set the instrument value to use later (this is only in cache)
         quant.setValue(value)
 
         # set DAQ module nodes
         if "DAQ" in quant.name:
             if quant.set_cmd:
-                # if a 'set_cmd' is defined, use 'set' of module
                 value = self._set_daq_value(quant, value)
             if "DAQ Trigger - Trigger" in quant.name:
-                # set the trigger signal
                 self._get_daq_trigger()
             if "DAQ Signal" in quant.name:
-                # set the DAQ signals
                 self._get_daq_signals()
             if quant.name == "DAQ - Measure" and value:
-                # start the measurement when button clicked (for 'value=True')
                 if not self.controller.daq.signals:
-                    # update signals if not defined
                     self._get_daq_signals()
                 self._daq_measure()
         # set Sweeper module nodes
         elif "Sweeper" in quant.name:
             if quant.set_cmd:
-                # if a 'set_cmd' is defined, use 'set' of module
                 value = self._set_sweeper_value(quant, value)
             if quant.name == "Sweeper - Parameter":
-                # change the sweep parameter
                 self.controller.sweeper.sweep_parameter(value)
             if quant.name == "Sweeper Advanced - Application":
-                # change the application preset
                 self.controller.sweeper.application(value)
             if "Sweeper Signal" in quant.name:
-                # set the Sweeper signals
                 self._get_sweeper_signals()
             if quant.name == "Sweeper Control - Measure" and value:
-                # start the measurement when button clicked (for 'value=True')
                 if not self.controller.sweeper.signals:
-                    # update signals if not defined
                     self._get_sweeper_signals()
                 self._sweeper_measure()
-        # if a 'set_cmd' is defined, just set the node
         elif quant.set_cmd:
             value = self._set_node_value(quant, value)
-        # return the value that was set on the device ...
         return value
 
     def performGetValue(self, quant, options={}):
         """Perform the Set Value instrument operation. This function should
         return the actual value set by the instrument"""
-        # getting a value from the DAQ module
         if "DAQ" in quant.name:
-            # if a 'get_cmd' is defined, use module to return the node value
             if quant.get_cmd:
                 return self.get_daq_value(quant)
-            # if we read a signal trace
             elif "Trace" in quant.name:
                 i = int(quant.name[11]) - 1
                 try:
-                    # get the corresponding result data
                     signal = self.controller.daq.signals[i]
                     result = self.controller.daq.results[signal]
                     return self._daq_result_to_quant(quant, result)
                 except:
-                    # if it's not there, return a zero array
                     return self._daq_return_zeros(quant)
-        # getting a value from the Sweeper module
         elif "Sweeper" in quant.name:
-            # if a 'get_cmd' is defined, use module to return the node value
             if quant.get_cmd:
                 return self._get_sweeper_value(quant)
-            # if we read a signal trace
             elif "Trace" in quant.name:
                 i = int(quant.name[15]) - 1
                 try:
-                    # get the corresponding result data
                     signal = self.controller.sweeper.signals[i]
                     result = self.controller.sweeper.results[signal]
                     return self._sweeper_result_to_quant(quant, result)
                 except:
-                    # if it's not there, return a zero array
                     return self._sweeper_return_zeros(quant)
-        # if not a DAQ or Sweeper node and a 'get_cmd' is defined
         elif quant.get_cmd:
             return self.controller._get(quant.get_cmd)
         else:
-            # otherwise return the cached value
             return quant.getValue()
 
     def _set_node_value(self, quant, value):
@@ -169,21 +140,14 @@ class Driver(LabberDriver):
 
     def _daq_result_to_quant(self, quant, result):
         """Gets the corresponding result data from the DAQ module."""
-        # FFT quantity but result is not from FFT signal
         if "FFT" in quant.name and result.frequency is None:
-            # return zeros
             return self._daq_return_zeros(quant)
-        # time domain quantity but result is not from time domain signal
         elif "FFT" not in quant.name and result.time is None:
-            # return zeros
             return self._daq_return_zeros(quant)
-        # other cases, i.e. where signal and result match
         else:
-            # get result data for FFT or time domain
             x = result.time if result.time is not None else result.frequency
             # select only first row, 2D arrays not supported in Labber!
             y = result.value[0]
-            # return as Labber trace dictionary
             return quant.getTraceDict(y, x=x)
 
     def _daq_return_zeros(self, quant):
@@ -196,20 +160,16 @@ class Driver(LabberDriver):
         base = quant.name[:19]
         signal_source = self.getValue(base + "Source")
         if "demod" in signal_source:
-            # for a demod result
             signal_type = self.getValue(base + "Type")
             operation = self.getValue(base + "Operation").replace("none", "")
             param = signal_type + operation
             x = result.grid
             try:
-                # try if selected attribute exists ...
                 y = result.__dict__[param]
             except KeyError:
-                # ... otherwise return zeros
                 return self._sweeper_return_zeros(quant)
             return quant.getTraceDict(y, x=x)
         else:
-            # if not a demod result just return 'value' attribute
             x = result.grid
             try:
                 y = result.value
@@ -225,12 +185,9 @@ class Driver(LabberDriver):
     def _get_daq_signals(self):
         """Add selected signals to measurement on DAQ module."""
         n_signals = int(self.getValue("DAQ Signals - Number of Signals"))
-        # clear signals list
         self.controller.daq.signals_clear()
-        # iterate through slected signals
         for i in range(n_signals):
             base = f"DAQ Signal {i+1} - "
-            # get signal source, type, operation, fft, ...
             signal_source = self.getValue(base + "Source")
             if "demod" in signal_source:
                 signal_type = self.getValue(base + "Type Demod")
@@ -239,7 +196,6 @@ class Driver(LabberDriver):
             operation = self.getValue(base + "Operation")
             fft = self.getValue(base + "FFT")
             selector = self.getValue(base + "Complex Selector")
-            # add selected signal to measurement
             s = self.controller.daq.signals_add(
                 signal_source,
                 signal_type,
@@ -247,26 +203,19 @@ class Driver(LabberDriver):
                 fft=fft,
                 complex_selector=selector,
             )
-            self.log(f"####   DAQ:    added signal '{s}'")
 
     def _get_sweeper_signals(self):
         """Add selected signals to measurement on Sweeper module."""
         n_signals = int(self.getValue("Sweeper Signals - Number of Signals"))
-        # clear signals list
         self.controller.sweeper.signals_clear()
-        # iterate through slected signals
         for i in range(n_signals):
             base = f"Sweeper Signal {i+1} - "
-            # get signal source, type, operation, fft, ...
             signal_source = self.getValue(base + "Source")
-            # add selected signal to measurement
             s = self.controller.sweeper.signals_add(signal_source)
-            self.log(f"####   Sweeper:    added signal '{s}'")
 
     def _get_daq_trigger(self):
         """Set selected trigger signal on DAQ module."""
         base = "DAQ Trigger - Trigger "
-        # get trigger source and type
         trigger_source = self.getValue(base + "Source")
         if "demod" in trigger_source:
             trigger_type = self.getValue(base + "Type Demod")
@@ -274,12 +223,7 @@ class Driver(LabberDriver):
             trigger_type = self.getValue(base + "Type Aux")
         elif "imp" in trigger_source:
             trigger_type = self.getValue(base + "Type Imp")
-        self.log(f"#####            gridnode: {trigger_source}, {trigger_type}")
-        # and set trigger
         self.controller.daq.trigger(trigger_source, trigger_type)
-        self.log(
-            f"#####            gridnode: {self.controller.daq._get('triggernode')}"
-        )
 
     def _daq_measure(self):
         """Start the measurement on the DAQ."""
@@ -289,8 +233,6 @@ class Driver(LabberDriver):
             self.controller.daq.measure(timeout=timeout)
             self.setValue("DAQ - Status", f"Ready for Measurement")
         except TimeoutError:
-            self.log(f"Measurement timed out!")
-            # reset results ...
             self.controller.daq._results = {}
 
     def _sweeper_measure(self):
@@ -304,6 +246,4 @@ class Driver(LabberDriver):
             self.controller.sweeper.measure(timeout=timeout)
             self.setValue("Sweeper Control - Status", "Ready for Measurement")
         except TimeoutError:
-            self.log(f"Measurement timed out!")
-            # reset results ...
             self.controller.sweeper._results = {}
--- Zurich_Instruments_UHFQA/Zurich_Instruments_UHFQA.py
+++ Zurich_Instruments_UHFQA/Zurich_Instruments_UHFQA.py
@@ -14,7 +14,6 @@ class Driver(LabberDriver):
 
     def performOpen(self, options={}):
         """Perform the operation of opening the instrument connection"""
-        # get the interface selected in UI, restrict to either 'USB' or '1GbE'
         interface = self.comCfg.interface
         if not interface == "USB":
             interface = "1GbE"
@@ -24,7 +23,6 @@ class Driver(LabberDriver):
         )
         self.controller.setup()
         self.controller.connect_device()
-        # initialize list for 'replace waveform'
         self.last_length = [0] * 2
 
     def performClose(self, bError=False, options={}):
@@ -39,29 +37,18 @@ class Driver(LabberDriver):
         """Perform the Set Value instrument operation. This function should
         return the actual value set by the instrument"""
 
-        # set the instrument value to use later (this is only in cache)
         quant.setValue(value)
 
-        # Initialize these lists at the start of 'Set Cfg' to keep track of
-        # what is changed, only compile or uplaod at the end of 'Set Cfg'
-        # see 'if self.isFinalCall(): ...' below
         if self.isFirstCall(options):
             self.sequencer_updated = False
             self.waveforms_updated = [False] * 2
             self.replace_waveform = False
 
-        # see if we are hardware-looping
         loop_index, n_HW_loop = self.getHardwareLoopIndex(options)
 
         # if a 'set_cmd' is defined, just set the node
         if quant.set_cmd:
             value = self.set_node_value(quant, value)
-
-        # go through all the quantity names that dont have 'set_cmd'
-        #
-        # do not use 'elif' or 'switch' here!
-        # for some quantities multiple 'ifs' are true, e.g. to mark the
-        # sequencer as updated
 
         # sequencer outputs
         if quant.name == "Control - Output 1":
@@ -86,7 +73,7 @@ class Driver(LabberDriver):
             self.controller.integration_time(value)
             value = self.controller.integration_time()
 
-        # sequencer start / stop (also a 'BUTTON' can be 'value=False')
+        # sequencer start / stop
         if quant.name.endswith("Run"):
             value = self.awg_start_stop(quant, value)
 
@@ -121,11 +108,8 @@ class Driver(LabberDriver):
 
         # sequencer waveform (for 'Simple' sequence)
         if quant.name.startswith("Waveform"):
-            # which channel?
             ch = int(quant.name[-1]) - 1
-            # mark the respective waveform as updated
             self.waveforms_updated[ch] = True
-            # make as 'replace waveform' if not HW looping
             if not self.isHardwareLoop(options):
                 data = value["y"]
                 self.replace_waveform = (
@@ -133,10 +117,6 @@ class Driver(LabberDriver):
                 )
                 self.last_length[ch] = len(data)
 
-        # at final call of 'Set Cfg' check if sequencer need to be
-        # - updated, i.e. call `awg.set_sequence_params(...)` with new values
-        # - queue new waveform
-        # - compiled, i.e. call `awg.compile()`
         if self.isFinalCall(options):
             if self.sequencer_updated:
                 # if any sequencers are marked as updated
@@ -146,12 +126,10 @@ class Driver(LabberDriver):
                 # queue the waveforms marked as updated to the respective AWG
                 self.queue_waveforms(options=options)
             if any(self.waveforms_updated) or self.sequencer_updated:
-                # sequencer needs to be recompiled if
-                # - a new waveform was queued
-                # - OR a sequence parameter was changed
+                # sequencer needs to be recompiled
                 if loop_index == 0:
-                    # this if statement is different from HDAWG TODO: check!
                     self.compile_sequencers()
+
         # return the value that was set on the device ...
         return value
 
@@ -165,73 +143,56 @@ class Driver(LabberDriver):
             # get the result vector
             i = int(quant.name[-2:]) - 1
             value = self.controller.channels[i].result()
-            # return as a Labber trace dictionary
             return quant.getTraceDict(value, x0=0, dx=1)
         elif quant.name.startswith("Result Avg - QB"):
             # get the _averaged_ result vector
             i = int(quant.name[-2:]) - 1
             value = self.controller.channels[i].result()
             if self.isHardwareLoop(options):
-                # if HW-looping, return elements in vector for each step
                 index, _ = self.getHardwareLoopIndex(options)
                 return value[index]
             else:
-                # otherwise return averaged value
                 return np.mean(value)
         elif quant.name == "Result Demod 1-2":
             # calculate 'demod 1-2' value
             return self.get_demod_12()
         else:
-            # otherwise return the cached value
             return quant.getValue()
 
     def performArm(self, quant_names, options={}):
         """Perform the instrument arm operation"""
         if self.getValue("QA Results - Enable"):
-            # if QA Results is used, start and arm the UHFQA results
             self.controller._set("/qas/0/result/enable", 1)
             self.controller.arm()
         if self.getValue("Sequencer - Trigger Mode") == "External Trigger":
-            # if AWG is used as slave, start the AWG
             self.controller.awg.run()
-            self.log(f"###       UHFQA: RUN SEQUENCER")
 
     def set_node_value(self, quant, value):
         if quant.datatype == quant.COMBO:
-            # for 'COMBO' quantities, set the defined command value
             i = quant.getValueIndex(value)
             if len(quant.cmd_def) == 0:
                 self.controller._set(quant.set_cmd, i)
             else:
                 self.controller._set(quant.set_cmd, quant.cmd_def[i])
         else:
-            # for all other datatypes, just set
             self.controller._set(quant.set_cmd, value)
         return self.controller._get(quant.get_cmd)
 
     def awg_start_stop(self, quant, value):
         """Handles setting of nodes with 'set_cmd'."""
-        # get the AWG index from the name
         if value:
-            # start the AWG
             self.controller.awg.run()
-            self.log(f"###       UHFQA: RUN SEQUENCER")
         else:
-            # stop the AWG
             self.controller.awg.stop()
         if self.controller._get("awgs/0/single"):
-            # wait until the AWG is finished
             self.controller.awg.wait_done()
         return self.controller.awg.is_running
 
     def update_sequencers(self):
         """Handles the 'set_sequence_params(...)' for the AWGs."""
-        # iterate through the AWGs
         if self.sequencer_updated:
-            # get all sequence parameters from Labber ...
             params = self.get_sequence_params()
             if params["sequence_type"] != "None":
-                # ... and set on the AWG if used
                 self.controller.awg.set_sequence_params(**params)
 
     def get_sequence_params(self):
@@ -261,44 +222,31 @@ class Driver(LabberDriver):
 
     def queue_waveforms(self, options={}):
         """Queue waveforms or replace waveforms on AWGs."""
-        # get the loop index
         loop_index, _ = self.getHardwareLoopIndex(options)
         if loop_index == 0 and not self.replace_waveform:
-            # reset queue at beginning
             self.controller.awg.reset_queue()
         if any(self.waveforms_updated):
-            # if one waveform is updated, get the arrays
             w1 = self.getValueArray("Waveform 1")
             w2 = self.getValueArray("Waveform 1")
             if self.replace_waveform:
-                # replace the waveform data if both are marked as 'replace'
                 self.controller.awg.replace_waveform(w1, w2, i=loop_index)
-                self.log(f"###       UHFQA: REPLACED WAVE {loop_index+1}")
             else:
-                # else queue the waveforms to the AWG
                 self.controller.awg.queue_waveform(w1, w2)
 
     def compile_sequencers(self):
         """Handles the compilation and waveform upload of the AWGs."""
-        # iterate over AWGs
         sequence_type = self.getValue("Sequencer - Sequence")
         if sequence_type != "None" and not self.replace_waveform:
-            # compile if AWG is used
             self.controller.awg.compile()
-            self.log(f"###       UHFQA: COMPILED SEQUENCER")
         if sequence_type == "Simple":
-            # if using a 'Simple' sequence, upload waveforms in the queue
             self.controller.awg.upload_waveforms()
-            self.log(f"###       UHFQA: UPLOADED WAVES")
 
     def set_cosstalk_matrix(self, matrix):
         """Set the crosstalk matrix as a 2D numpy array."""
         rows, cols = matrix.shape
-        # set the Labber quantities
         for r in range(rows):
             for c in range(cols):
                 self.setValue(f"Crosstalk - {r+1} , {c+1}", matrix[r, c])
-        # set the instrument value
         self.controller.crosstalk_matrix(matrix)
 
     def get_demod_12(self):
