808c587c8df9a60fe3d6232582aabaafaf3207f8
Tobias Ahrens <tobias.ahrens@zhinst.com>
2022-03-10 09:01:22 +0100

    Add function calls to base driver

--- src/zhinst/labber/base_instrument.py
+++ src/zhinst/labber/base_instrument.py
@@ -1,7 +1,8 @@
 import typing as t
-import json
 from enum import Enum
-
+from pathlib import Path
+from itertools import repeat
+import inspect
 
 from BaseDriver import LabberDriver
 from InstrumentDriver_Interface import Interface
@@ -11,79 +12,44 @@ from zhinst.toolkit.driver.devices import DeviceType
 from zhinst.toolkit.driver.modules import ModuleType
 
 from zhinst.labber.snapshot_manager import SnapshotManager
+from zhinst.labber.functions import execute_function, FUNCTIONPOSTFIX
 
 Quantity = t.TypeVar("Quantity")
 
 created_sessions = {}
 
+
 class BaseType(Enum):
     """Supported Instrument Types"""
 
-    SESSION = 0,
-    MODULE = 1,
-    DEVICE = 2,
+    SESSION = 0
+    MODULE = 1
+    DEVICE = 2
+
 
 class BaseDevice(LabberDriver):
     """Implement a Labber Driver for the Zurich Instruments SHFSG"""
 
-    def __init__(self, *args, settings_file, **kwargs):
+    def __init__(self, *args, settings, **kwargs):
         super().__init__(*args, **kwargs)
         self._session = None
         self._instrument = None
         self._base_type = None
         self._transaction = None
         self._snapshot = None
+        self._settings = settings
 
-        with settings_file.open("r") as file:
-            self._settings = json.loads(file.read())
-
-    def _get_session(self, data_server_info:t.Dict[str, t.Any]) -> Session:
-        target_host = data_server_info.get("host", "localhost")
-        target_hf2 = data_server_info.get("hf2", False)
-        target_port = data_server_info.get("port", 8005 if target_hf2 else 8004)
-        if data_server_info.get("shared_session", True):
-            for (host, port), session in created_sessions.items():
-                if target_host == host and target_port == port:
-                    return session
-        new_session = Session(target_host, target_port, hf2=target_hf2)
-        created_sessions[(target_host, target_port)] = new_session
-        return new_session
-
-    def _create_instrument(self, instrument_info:t.Dict[str, t.Any]) -> t.Union[Session, DeviceType, ModuleType]:
-        base_type = instrument_info.get(base_type, "session")
-        if base_type == "session":
-            self._base_type = BaseType.SESSION
-            return self._session
-        if base_type == "module":
-            self._base_type = BaseType.MODULE
-            try:
-                return getattr(self._session.modules, instrument_info["type"].lower())
-            except KeyError as error:
-                raise RuntimeError(
-                    "Settingsfile is specifing a module as instrument but is "
-                    "missing the \"type\" property."
-                    ) from error
-            except AttributeError as error:
-                raise RuntimeError(
-                    f"LabOne module with name {instrument_info['type'].lower()}"
-                    " does not exist in toolkit."
-                )
-        self._base_type = BaseType.DEVICE
-        return self._session.connect_device(self.comCfg.getAddressString())
-
-
-    def performOpen(self, options={}):
+    def performOpen(self, options: t.Dict = {}) -> None:
         """Perform the operation of opening the instrument connection."""
-
         self._session = self._get_session(self._settings["data_server"])
         self._instrument = self._create_instrument(self._settings["instrument"])
         self._snapshot = SnapshotManager(self._instrument.root)
 
-    def performClose(self, bError=False, options={}):
+    def performClose(self, bError: bool = False, options: t.Dict = {}) -> None:
         """Perform the close instrument connection operation."""
         pass
 
-    def initSetConfig(self):
+    def initSetConfig(self) -> None:
         """Run before setting values in Set Config."""
         pass
 
@@ -137,22 +103,11 @@ class BaseDevice(LabberDriver):
             # Normal set of value
             return self._set_value_toolkit(quant, value, options=options)
         # Quants without a set_cmd ?
+        if FUNCTIONPOSTFIX in quant.name:
+            execute_function(quant, self._instrument, self.dQuantities)
         return value
 
-    def _set_value_toolkit(
-        self, quant: Quantity, value: t.Any, *, options: t.Dict = {}
-    ) -> None:
-        if "call_no" in options and not self._transaction:
-            # start transaction
-            self._transaction = self._instrument.set_transaction
-            self._transaction.__enter__()
-        self._instrument[quant.set_cmd](value)
-        if self._transaction and self.isFinalCall():
-            # end transaction
-            self._transaction.__exit__()
-            self._transaction = None
-
-    def performGetValue(self, quant, options={}):
+    def performGetValue(self, quant: Quantity, options: t.Dict = {}):
         """Perform the Get Value instrument operation."""
         if quant.get_cmd:
             if self.dOp["operation"] == Interface.GET_CFG:
@@ -162,17 +117,59 @@ class BaseDevice(LabberDriver):
             self._snapshot.clear()
             return self._instrument[quant.get_cmd]()
         # TODO quant without a get_cmd
-        return None
+        print(f"DEBUG: Quant: {quant.name} does not have a get_cmd")
+        return quant.getValue()
 
-    def performArm(self, quant_names, options={}):
+    def performArm(self, quant_names: str, options: t.Dict = {}) -> None:
         """Perform the instrument arm operation"""
         # TODO
 
-    def update_from_session(self, quant_name: str) -> None:
-        """Read the value from the session and update the Labber controller.
+    def _set_value_toolkit(
+        self, quant: Quantity, value: t.Any, *, options: t.Dict = {}
+    ) -> None:
+        if "call_no" in options and not self._transaction:
+            # start transaction
+            self._transaction = self._instrument.set_transaction
+            self._transaction.__enter__()
+        # TODO device if it should be deep set or not
+        self._instrument[quant.set_cmd](value)
+        if self._transaction and self.isFinalCall():
+            # end transaction
+            self._transaction.__exit__()
+            self._transaction = None
 
-        Args:
-            quant_name: Name of the quant that should be updated
-        """
-        value = self.readValueFromOther(quant_name)
-        self.setValue(quant_name, value)
+    def _get_session(self, data_server_info: t.Dict[str, t.Any]) -> Session:
+        target_host = data_server_info.get("host", "localhost")
+        target_hf2 = data_server_info.get("hf2", False)
+        target_port = data_server_info.get("port", 8005 if target_hf2 else 8004)
+        if data_server_info.get("shared_session", True):
+            for (host, port), session in created_sessions.items():
+                if target_host == host and target_port == port:
+                    return session
+        new_session = Session(target_host, target_port, hf2=target_hf2)
+        created_sessions[(target_host, target_port)] = new_session
+        return new_session
+
+    def _create_instrument(
+        self, instrument_info: t.Dict[str, t.Any]
+    ) -> t.Union[Session, DeviceType, ModuleType]:
+        base_type = instrument_info.get("base_type", "session")
+        if base_type == "session":
+            self._base_type = BaseType.SESSION
+            return self._session
+        if base_type == "module":
+            self._base_type = BaseType.MODULE
+            try:
+                return getattr(self._session.modules, instrument_info["type"].lower())
+            except KeyError as error:
+                raise RuntimeError(
+                    "Settingsfile is specifing a module as instrument but is "
+                    'missing the "type" property.'
+                ) from error
+            except AttributeError as error:
+                raise RuntimeError(
+                    f"LabOne module with name {instrument_info['type'].lower()}"
+                    " does not exist in toolkit."
+                )
+        self._base_type = BaseType.DEVICE
+        return self._session.connect_device(self.comCfg.getAddressString())
---
+++ src/zhinst/labber/functions.py
@@ -0,0 +1,144 @@
+"""Translates between Labber and a toolkit function."""
+from pathlib import Path
+import numpy as np
+from enum import Enum
+import csv
+from itertools import repeat
+import inspect
+import typing as t
+
+
+from zhinst.toolkit import Waveforms
+
+NumpyArray = t.TypeVar("NumpyArray")
+Instrument = t.TypeVar("Instrument")
+
+FUNCTIONPOSTFIX = "EXECUTEFUNC"
+
+
+class QuantType(Enum):
+    """Supported quant types Types."""
+
+    GENERIC = 0
+    SEQUENCER = 1
+    WAVEFORM = 2
+
+
+def _csv_row_to_vector(csv_row: list[str]) -> t.Optional[NumpyArray]:
+    """Convert a csv row into a numpy array.
+
+    Args:
+        csv_row: Parsed CSV row
+    Returns:
+        Numpy array.
+    """
+    if not csv_row:
+        return None
+    datatype = type(eval(csv_row[0]))
+    return np.array(csv_row[:-1], dtype=datatype.__name__)
+
+
+def _import_waveforms(
+    wave1: Path, wave2: Path = None, marker: Path = None
+) -> Waveforms:
+    """Import Waveforms from CSV files.
+
+    Args:
+        wave1: csv for real part waves
+        wave2: csv for imag part waves
+        marker: csv for markers
+
+    Returns:
+        Waveform object
+    """
+    wave1_reader = csv.reader(wave1.open("r", newline=""), delimiter=",", quotechar="|")
+    wave2_reader = repeat([])
+    if wave2 and wave2.exists():
+        wave2_reader = csv.reader(
+            wave2.open("r", newline=""), delimiter=",", quotechar="|"
+        )
+    marker_reader = repeat([])
+    if marker and marker.exists():
+        marker_reader = csv.reader(
+            marker.open("r", newline=""), delimiter=",", quotechar="|"
+        )
+
+    waves = Waveforms()
+    for i, row in enumerate(zip(wave1_reader, wave2_reader, marker_reader)):
+        if not row[0]:
+            continue
+        waves[i] = (
+            _csv_row_to_vector(row[0]),
+            _csv_row_to_vector(row[1]),
+            _csv_row_to_vector(row[2]),
+        )
+    return waves
+
+
+def get_waveform_arg_names(function):
+    arg_names = []
+    for key, value in inspect.signature(function).parameters.items():
+        if "waveform.Waveforms" in str(value.annotation):
+            arg_names.append(key)
+    return arg_names
+
+def _process_function_returns(function, *args):
+    name = function.__name__
+    if name == "test":
+        return None
+    return args
+
+def execute_function(
+    function_group: str, instrument: Instrument, all_quants: t.List
+) -> t.Any:
+    """Execute a toolkit function.
+
+    Args:
+        function_group: Labber group of the function.
+        instrument: instrument of the function.
+        all_quants: all available quants.
+    """
+
+    # get function object
+    function = instrument
+    for name in function_group.lower().split(" - "):
+        if name.isnumeric():
+            function = function[int(name)]
+        else:
+            function = getattr(function, name)
+
+    # get arguments
+    arg_quant = []
+    waveform_count = 0
+    for other_quant in all_quants.values():
+        if function_group == other_quant.group and FUNCTIONPOSTFIX not in name:
+            quant_type = QuantType.GENERIC
+            if other_quant.datatype == other_quant.PATH:
+                if "csv" in other_quant.set_cmd:
+                    quant_type = QuantType.WAVEFORM
+                    waveform_count += 1
+                else:
+                    quant_type = QuantType.SEQUENCER
+            arg_quant.append[(other_quant, quant_type)]
+
+    if waveform_count not in [0, 1, 3]:
+        raise RuntimeError(f"Invalid amount of waveform paths. {waveform_count}")
+
+    # get argument values
+    kwargs = {}
+    waveform_paths = []
+    for other_quant, quant_type in arg_quant:
+        arg_name = other_quant.name.lower().split(" - ")[-1]
+        if quant_type == QuantType.SEQUENCER:
+            with Path(other_quant.getValue()).open("r") as file:
+                kwargs[arg_name] = file.read()
+        elif quant_type == QuantType.WAVEFORM:
+            waveform_paths.append(Path(other_quant.getValue()))
+            if len(waveform_paths) == waveform_count:
+                # TODO only one Waveform argument per function call supported
+                arg_names = get_waveform_arg_names(function)
+                kwargs[arg_names[0]] = _import_waveforms(*waveform_paths)
+        else:
+            kwargs[arg_name] = other_quant.getValue()
+    return _process_function_returns(function, function(**kwargs))
+
