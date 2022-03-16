a842f504e86957879fb467512cf197fe6f668ce3
Markus Heikkinen <markus.heikkinen@zhinst.com>
2022-03-16 17:38:44 +0100

    test_cli_script to utilize tempdir in tests

--- tests/generator/test_cli_script.py
+++ tests/generator/test_cli_script.py
@@ -1,3 +1,4 @@
+import tempfile
 from unittest import mock
 import pytest
 
@@ -20,10 +21,9 @@ def test_cli_script_setup_help():
 @mock.patch("zhinst.labber.cli_script.generate_labber_files")
 @pytest.mark.parametrize("inp, outp", [
     (
-        ["setup", "../tests", "dev1234", "localhost"], 
+        ["dev1234", "localhost"], 
         {
             "device": "dev1234",
-            "filepath": "../tests",
             "hf2": False,
             "mode": "NORMAL",
             "server_host": "localhost",
@@ -32,10 +32,9 @@ def test_cli_script_setup_help():
             }
     ),
     (
-        ["setup", "../tests", "dev1234", "localhost", "--server_port=812", "--hf2", "--upgrade", "--mode=ADVANCED"], 
+        ["dev1234", "localhost", "--server_port=812", "--hf2", "--upgrade", "--mode=ADVANCED"], 
         {
             "device": "dev1234",
-            "filepath": "../tests",
             "hf2": True,
             "mode": "ADVANCED",
             "server_host": "localhost",
@@ -44,10 +43,9 @@ def test_cli_script_setup_help():
             }
     ),
     (
-        ["setup", "../tests", "dev1234", "localhost", "--server_port=812"], 
+        ["dev1234", "localhost", "--server_port=812"], 
         {
             "device": "dev1234",
-            "filepath": "../tests",
             "hf2": False,
             "mode": "NORMAL",
             "server_host": "localhost",
@@ -58,32 +56,31 @@ def test_cli_script_setup_help():
 ])
 def test_cli_script_setup(mock_gen, inp, outp):
     runner = CliRunner()
-    result = runner.invoke(main, inp)
-    mock_gen.assert_called_with(**outp)
-    assert result.exit_code == 0
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        result = runner.invoke(main, ['setup', tmpdirname] + inp)
+        outp['filepath'] = tmpdirname
+        mock_gen.assert_called_with(**outp)
+        assert result.exit_code == 0
 
 
 @mock.patch("zhinst.labber.cli_script.generate_labber_files")
-@pytest.mark.parametrize("inp, err_code, output", [
-    (
-        ["setup"], 
-        2,
-        "Missing argument 'FILEPATH'."
-    ),
-    (
-        ["setup", "../tests"], 
-        2,
-        "Missing argument 'DEVICE'."
-    ),
-    (
-        ["setup", "../tests", "dev1234"], 
-        2,
-        "Error: Missing argument 'SERVER_HOST'."
-    ),
-])
-def test_cli_script_setup_errors(mock_gen, inp, err_code, output):
+def test_cli_script_setup_errors(mock_gen):
     runner = CliRunner()
-    result = runner.invoke(main, inp)
-    mock_gen.assert_not_called()
-    assert result.exit_code == err_code
-    assert output in result.output
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        command = ["setup"]
+        result = runner.invoke(main, command)
+        mock_gen.assert_not_called()
+        assert result.exit_code == 2
+        assert "Missing argument 'FILEPATH'." in result.output
+
+        command = ["setup", tmpdirname]
+        result = runner.invoke(main, command)
+        mock_gen.assert_not_called()
+        assert result.exit_code == 2
+        assert "Missing argument 'DEVICE'." in result.output
+        
+        command = ["setup", tmpdirname, "dev123"]
+        result = runner.invoke(main, command)
+        mock_gen.assert_not_called()
+        assert result.exit_code == 2
+        assert "Error: Missing argument 'SERVER_HOST'." in result.output
