fac86e9c566030f10358fe14b1dd159e8ea51bf8
Markus Heikkinen <markus.heikkinen@zhinst.com>
2022-03-16 16:45:33 +0100

    JSON schema for settings.json testing

--- tests/test_settings_json.py
+++ tests/test_settings_json.py
@@ -1,17 +1,19 @@
-def test_common_keys_exists(settings_json):
-    assert isinstance(settings_json['common']['ignoredNodes']['normal'], list)
-    assert isinstance(settings_json['common']['ignoredNodes']['advanced'], list)
-    assert isinstance(settings_json['common']['ignoredNodes']['normal'], list) 
-    assert isinstance(settings_json['common']['generalSettings'], dict) 
+import jsonschema
+import pytest
+from pathlib import Path
+import json
 
-def test_misc_keys(settings_json):
-    assert isinstance(settings_json["misc"]['labberDelimiter'], str)
-    assert isinstance(settings_json["misc"]['ziModules'], list)
 
-def test_common_mapping(settings_json):
-    for v in settings_json['common']['quants'].values():
-        if "mapping" in v.keys():
-            assert "indexes" not in v.keys()
-            for k in v["mapping"].values():
-                assert "path" in k.keys()
-                assert "indexes" in k.keys()
+@pytest.fixture
+def settings_json_schema():
+    settings_file = Path(__file__).parent.parent / "src" / "zhinst" /"labber" / "settings_json_schema.json"
+    with open(settings_file, "r") as json_f:
+        return json.load(json_f)
+
+
+def test_validate_json(settings_json_schema, settings_json):
+    jsonschema.validate(
+        instance=settings_json,
+        schema=settings_json_schema,
+        cls=jsonschema.Draft4Validator,
+    )
